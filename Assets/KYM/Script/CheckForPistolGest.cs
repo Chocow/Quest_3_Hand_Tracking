1f62149374b50cbb5d70531bfb2243259b553b33
MtK50 <75019995+MtK50@users.noreply.github.com>
2025-11-19 14:38:23 +0100

    A tester Pistolet gesture en scene

---
+++ Assets/KYM/Script/CheckForPistolGest.cs
@@ -0,0 +1,66 @@
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+using UnityEngine.XR.Hands.Gestures;
+
+
+namespace UnityEngine.XR.Hands.Samples.GestureSample
+{
+
+    public class CheckForPistolGest : MonoBehaviour
+    {
+
+        [SerializeField] XRHandTrackingEvents m_HandTrackingEvents;
+
+
+        [SerializeField] ScriptableObject m_HandShapeOrPose;
+        XRHandShape m_HandShape;
+        XRHandPose m_HandPose;
+        bool m_WasDetected;
+        bool m_PerformedTriggered;
+        float m_TimeOfLastConditionCheck;
+        float m_HoldStartTime;
+
+        void OnEnable()
+        {
+            m_HandTrackingEvents.jointsUpdated.AddListener(OnJointsUpdated);
+
+            m_HandShape = m_HandShapeOrPose as XRHandShape;
+        }
+        void OnDisable()
+        {
+            m_HandTrackingEvents.jointsUpdated.RemoveListener(OnJointsUpdated);
+        }
+
+        bool m_ExecutedOnce = false;
+
+        void OnJointsUpdated(XRHandJointsUpdatedEventArgs eventArgs)
+        {
+            if (!isActiveAndEnabled)
+                return;
+
+            var detected =
+                m_HandTrackingEvents.handIsTracked &&
+                m_HandShape != null && m_HandShape.CheckConditions(eventArgs) ||
+                m_HandPose != null && m_HandPose.CheckConditions(eventArgs);
+
+            if (m_WasDetected && !detected)
+            {
+                m_PerformedTriggered = false;
+            }
+
+            m_WasDetected = detected;
+
+            if (!m_PerformedTriggered && detected)
+            {
+                m_PerformedTriggered = true;
+                ExecuteOnce();
+            }
+        }
+
+        void ExecuteOnce()
+        {
+            Debug.Log("Pistol gesture detected");
+        }
+    }
+}
